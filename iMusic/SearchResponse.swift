75ce61187d9fb08f104e00113112eb475a4f19f6
xubuntus <brief-lassos0j@icloud.com>
2024-01-18 22:28:10 +0300

    add itunes model

---
+++ iMusic/SearchResponse.swift
@@ -0,0 +1,20 @@
+//
+//  SearchResponse.swift
+//  iMusic
+//
+//  Created by nik on 18.01.2024.
+//
+
+import Foundation
+
+struct SearchResponse: Decodable {
+    var resultCount: Int
+    var results: [Track]
+}
+
+struct Track: Decodable {
+    var trackName: String
+    var collectionName: String?
+    var artistName: String
+    var artworkUrl100: String?
+}
