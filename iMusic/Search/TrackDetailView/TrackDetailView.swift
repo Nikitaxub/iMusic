364c1f59b4a523a5e654211c1d6d7e994c2d696d
xubuntus <brief-lassos0j@icloud.com>
2024-01-30 20:56:58 +0300

    add Track detail design

---
+++ iMusic/Search/TrackDetailView/TrackDetailView.swift
@@ -0,0 +1,16 @@
+//
+//  TrackDetailView.swift
+//  iMusic
+//
+//  Created by nik on 28.01.2024.
+//
+
+import UIKit
+
+class TrackDetailView: UIView {
+
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+}
