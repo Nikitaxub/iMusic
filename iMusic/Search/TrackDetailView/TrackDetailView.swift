257cafca2c90c01b64ed670aa1dac223a2d9f74a
xubuntus <brief-lassos0j@icloud.com>
2024-02-01 18:52:05 +0300

    add track playing

--- iMusic/Search/TrackDetailView/TrackDetailView.swift
+++ iMusic/Search/TrackDetailView/TrackDetailView.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import SDWebImage
+import AVKit
 
 class TrackDetailView: UIView {
     
@@ -18,8 +20,30 @@ class TrackDetailView: UIView {
     @IBOutlet weak var playPauseButton: UIButton!
     @IBOutlet weak var volumeSlider: UISlider!
     
+    let player: AVPlayer = {
+        let avPlayer = AVPlayer()
+        avPlayer.automaticallyWaitsToMinimizeStalling = false
+        return avPlayer
+    }()
+    
     override class func awakeFromNib() {
         super.awakeFromNib()
+    }
+    
+    func set(viewModel: SearchViewModel.Cell) {
+        trackTitleLabel.text = viewModel.trackName
+        authorTitleLabel.text = viewModel.artistName
+        playTrack(previewUrl: viewModel.previewUrl)
+        let string600 = viewModel.iconUrlString?.replacingOccurrences(of: "100x100", with: "600x600")
+        guard let url = URL(string: string600 ?? "") else { return}
+        trackImageView.sd_setImage(with: url)
+    }
+    
+    private func playTrack(previewUrl: String?) {
+        guard let url = URL(string: previewUrl ?? "") else { return }
+        let playerItem = AVPlayerItem(url: url)
+        player.replaceCurrentItem(with: playerItem)
+        player.play()
     }
     
     @IBAction func dragDownButtonTapped(_ sender: UIButton) {
@@ -36,6 +60,13 @@ class TrackDetailView: UIView {
     }
     
     @IBAction func playPauseAction(_ sender: UIButton) {
+        if player.timeControlStatus == .paused {
+            player.play()
+            playPauseButton.setImage(UIImage(named: "pause"), for: .normal)
+        } else {
+            player.pause()
+            playPauseButton.setImage(UIImage(named: "play"), for: .normal)
+        }
     }
     
     @IBAction func nextTrack(_ sender: UIButton) {
